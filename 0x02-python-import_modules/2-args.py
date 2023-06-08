650ccb45d1c90d9c780d7c74c68ae30c64669c7a
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-06-08 15:47:23 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     from sys import argv
-    count = 1
-
-    # excludes name of program
-    number_of_args = len(argv) - 1
-    if number_of_args == 0:
-        print(f"{number_of_args:d} arguments.")
-    if number_of_args == 1:
-        print(f"{number_of_args:d} argument:")
-    if number_of_args > 1:
-        print(f"{number_of_args:d} arguments:")
-    while count < len(argv):
-        print(f"{count:d}: {argv[count]}")
-        count += 1
+    l = len(argv)
+    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
+                                 "." if l == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
