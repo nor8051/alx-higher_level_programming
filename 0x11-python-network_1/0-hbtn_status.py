d43785f3db64c8260d531e421298530ddcb71b45
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-10-02 19:17:52 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Python script Fetches https://intranet.hbtn.io/status."""
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as req
+
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
-            as response:
-        html = response.read()
-
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
